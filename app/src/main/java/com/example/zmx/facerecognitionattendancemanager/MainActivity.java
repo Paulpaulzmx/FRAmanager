a7f1929d6bb4cf5409cd856f68ef630455d47782
Paulpaulzmx <980455605@qq.com>
2018-12-14 17:19:35 +0800

    3个碎片可以切换

--- app/src/main/java/com/example/zmx/facerecognitionattendancemanager/MainActivity.java
+++ app/src/main/java/com/example/zmx/facerecognitionattendancemanager/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.zmx.facerecognitionattendancemanager;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -12,30 +12,41 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
     private DrawerLayout drawerLayout;
+
+    private static final String CURRENT_FRAGMENT = "STATE_FRAGMENT_SHOW";
+
+    private FragmentManager fragmentManager;
+
+    private Fragment currentFragment = new Fragment();
+    private List<Fragment> fragments = new ArrayList<>();
+
+    private int currentIndex = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        replaceFragment(new HistoryFragment());
+        fragmentManager = getSupportFragmentManager();      //初始化fragmentManager
 
         //toolbar
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-//            actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.mipmap.toolbar_menu);
         }
@@ -43,8 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //drawer_layout抽屉布局
         drawerLayout = findViewById(R.id.drawer_layout);
         final NavigationView navView = findViewById(R.id.nav_view);
-        //设置抽屉中单项点击事件
+
 //        navView.setCheckedItem(R.id.nav_history);           //设置被选中的项目
+        //设置抽屉中单项点击事件
         navView.setNavigationItemSelectedListener(new NavigationView.
                 OnNavigationItemSelectedListener() {
             @Override
@@ -53,20 +65,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //根据id判定点击的按钮
                 switch (item.getItemId()) {
                     case R.id.nav_history:
-                        replaceFragment(new HistoryFragment());
-                        Snackbar.make(drawerLayout, "Data deleted", Snackbar.LENGTH_LONG)
-                                .setAction("撤销", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        Toast.makeText(MainActivity.this, "Data restored",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                }).show();
+                        currentIndex = 0;
+                        break;
+
+                    case R.id.nav_stu_list:
+                        currentIndex = 1;
+                        break;
+
+                    case R.id.nav_time_set:
+                        currentIndex = 2;
                         break;
 
                     default:
                         break;
                 }
+
+                showFragment();
+
                 return true;
             }
         });
@@ -76,16 +91,89 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FloatingActionButton flaSignature = findViewById(R.id.fab_signature);
         flaSignature.setOnClickListener(this);
 
+
+
+        if (savedInstanceState != null) {       //内存重启时调用
+
+            //获取“内存重启”时保存的索引下标
+            currentIndex = savedInstanceState.getInt(CURRENT_FRAGMENT,0);
+
+            //注意，添加顺序要跟下面添加的顺序一样！！！！
+            fragments.removeAll(fragments);
+            fragments.add(fragmentManager.findFragmentByTag(0+""));
+            fragments.add(fragmentManager.findFragmentByTag(1+""));
+            fragments.add(fragmentManager.findFragmentByTag(2+""));
+
+            //恢复fragment页面
+            restoreFragment();
+
+        }else{      //正常启动时调用
+
+            fragments.add(new HistoryFragment());
+            fragments.add(new StuListFragment());
+            fragments.add(new TimeSettingFragment());
+
+            showFragment();
+        }
     }
 
+//以下是fragment切换涉及到的逻辑
+    /**
+     * 使用show() hide()切换页面
+     * 显示fragment
+     */
+    private void showFragment() {
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment, fragment);
-        transaction.addToBackStack(null);       //按返回后返回到上一个Fragment
+
+        if(!fragments.get(currentIndex).isAdded()){      //之前没有被添加过
+            transaction
+                    .hide(currentFragment)
+                    .add(R.id.fragment,fragments.get(currentIndex),""+currentIndex);  //第三个参数为添加当前的fragment时绑定一个tag
+        }
+        else {
+            transaction
+                    .hide(currentFragment)
+                    .show(fragments.get(currentIndex));
+        }
+
+        currentFragment = fragments.get(currentIndex);
+
         transaction.commit();
     }
+
+
+    /**
+     * 恢复fragment
+     */
+    private void restoreFragment() {
+
+        FragmentTransaction mBeginTransaction = fragmentManager.beginTransaction();
+
+        for (int i = 0; i < fragments.size(); i++) {
+
+            if(i == currentIndex){
+                mBeginTransaction.show(fragments.get(i));
+            }else{
+                mBeginTransaction.hide(fragments.get(i));
+            }
+
+        }
+
+        mBeginTransaction.commit();
+
+        //把当前显示的fragment记录下来
+        currentFragment = fragments.get(currentIndex);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //“内存重启”时保存当前的fragment名字
+        outState.putInt(CURRENT_FRAGMENT,currentIndex);
+        super.onSaveInstanceState(outState);
+    }
+
 
     //按钮响应
     @Override
---
+++ app/src/main/java/com/example/zmx/facerecognitionattendancemanager/TimeSettingFragment.java
@@ -0,0 +1,16 @@
+package com.example.zmx.facerecognitionattendancemanager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TimeSettingFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_time_setting, container, false);
+        return view;
+    }
+}
